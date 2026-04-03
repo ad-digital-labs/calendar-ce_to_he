2cbd0980114796390a9beb35b5dd1d22f344c6d9
¨ad-digital-labs¨ <ad.digitallabs@atomicmail.io>
2026-04-03 20:35:37 +0800

    ¨proto¨

---
+++ a.cpp
@@ -0,0 +1,42 @@
+#define P_OK 0
+#define P_ERR_RANGE -10
+#define P_ERR_VAL -20
+
+
+#include <iostream>
+#include <time.h>
+#include <unistd.h>
+#include <string>
+#include <cctype>
+
+
+using namespace std;
+
+class caltime
+{
+    public:
+
+    
+
+    struct cal_ce
+    {
+        int mo;
+        int day;
+        int year;
+    };
+
+    struct cal_he
+    {
+        int mo;
+        int day;
+        int year;
+    };
+
+
+    
+
+
+
+
+
+};
